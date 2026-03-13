66644c65cee48596176f9c489e05fac5d45aa06f
OmryMuadi <omrymuadi2@gmail.com>
2026-03-13 17:49:21 +0200

    added logger to user service

---
+++ StockMarketServer/UserService/src/main/kotlin/logger/Logger.kt
@@ -0,0 +1,11 @@
+package logger
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object Logger {
+    private val logger: Logger = LoggerFactory.getLogger("User Service")
+    fun log(message: String){
+        logger.info(message)
+    }
+}
