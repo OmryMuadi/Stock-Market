3a5b9b259bd1732ed672bfedbb05f889d7b1fc4b
muadiom <muadiom@post.bgu.ac.il>
2026-03-31 10:58:02 +0300

    moving functions from trading service to transaction service

--- StockMarketServer/UserService/src/main/kotlin/dev/kourier/service/TransactionService.kt
+++ StockMarketServer/UserService/src/main/kotlin/dev/kourier/service/TransactionService.kt
@@ -11,7 +11,8 @@ import kotlin.math.PI
 
 
 @Service
-class TransactionService {
+class TransactionService(private val portfolioService: PortfolioService,
+                         private val latestPriceService: LatestPriceService) {
 
     val transactions: MutableMap<UUID, Transaction> = mutableMapOf()
     fun saveTransaction(type: TransactionType, userEmail: String, symbol: String, quantity: Int, currentPrice: Float) {
@@ -27,5 +28,86 @@ class TransactionService {
             }
         }
         return userTransactions
+    }
+
+    fun buyStock(userEmail: String, symbol: String, quantity: Int){
+        val portfolio = portfolioService.getPortfolio(userEmail)
+            ?: throw RuntimeException("Portfolio not found")
+
+        if(latestPriceService.prices[symbol] == null){
+            throw IllegalArgumentException("Stock with symbol $symbol not found")
+        }
+
+        val currentPrice = latestPriceService.getLastPrice(symbol)
+
+        val totalCost = quantity * currentPrice
+
+        if (portfolio.cashBalance < totalCost) {
+            throw RuntimeException("Insufficient funds")
+        }
+
+        portfolioService.decreaseCash(portfolio, totalCost)
+
+        val existingHolding = portfolio.holdings.find { it.symbol == symbol }
+
+        if (existingHolding != null) {
+            val oldQty = existingHolding.quantity
+            val oldAvg = existingHolding.averageBuyPrice
+
+            val totalOldInvestment = oldQty * oldAvg
+            val totalNewInvestment = quantity * currentPrice
+
+            val newQuantity = oldQty + quantity
+            val newAverageBuyPrice = (totalOldInvestment + totalNewInvestment) / newQuantity
+
+            portfolioService.updateHolding(portfolio, symbol, newQuantity, newAverageBuyPrice, currentPrice)
+        } else {
+            portfolioService.addHolding(portfolio, symbol, quantity, currentPrice)
+
+        }
+
+        saveTransaction(TransactionType.BUY, userEmail, symbol, quantity, currentPrice)
+        portfolioService.savePortfolio(portfolio)
+    }
+
+    fun sellStock(userEmail: String, symbol: String, quantity: Int){
+        val portfolio = portfolioService.getPortfolio(userEmail)
+            ?: throw RuntimeException("Portfolio not found")
+
+        if(latestPriceService.prices[symbol] == null){
+            throw IllegalArgumentException("Stock with symbol $symbol not found")
+        }
+
+        val existingHolding = portfolio.holdings.find { it.symbol == symbol } ?:
+        throw IllegalArgumentException("The user doesn't have stock with symbol $symbol")
+
+        if (existingHolding.quantity < quantity){
+            throw IllegalArgumentException("The required quantity for selling is more than how much the user has")
+        }
+
+        val currentPrice = latestPriceService.getLastPrice(symbol)
+
+        val totalCost = quantity * currentPrice
+
+        portfolioService.increaseCash(portfolio, totalCost)
+
+        val oldQty = existingHolding.quantity
+        val oldAvg = existingHolding.averageBuyPrice
+
+        existingHolding.quantity = existingHolding.quantity - quantity
+
+        if (existingHolding.quantity == 0){
+            portfolioService.removeHolding(portfolio, symbol)
+        }
+        else{
+            val totalOldInvestment = oldQty * oldAvg
+            val totalNewInvestment = existingHolding.quantity * currentPrice
+
+            val newQuantity = existingHolding.quantity
+            val newAverageBuyPrice = (totalOldInvestment + totalNewInvestment) / newQuantity
+            portfolioService.updateHolding(portfolio, symbol, newQuantity, newAverageBuyPrice, currentPrice)
+        }
+        saveTransaction(TransactionType.SELL, userEmail, symbol, quantity, currentPrice)
+        portfolioService.savePortfolio(portfolio)
     }
 }
